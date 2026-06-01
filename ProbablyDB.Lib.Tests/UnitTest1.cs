19828e0650dc25db4ac6c93020885113e1343293
senthil-omarchy <senthil.santhoshgopal@gmail.com>
2026-06-02 00:02:06 +1200

    scafolding

---
+++ ProbablyDB.Lib.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace ProbablyDB.Lib.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ ProbablyDB.Lib/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ProbablyDB.Lib
+{
+    public class Class1
+    {
+
+    }
+}
